49bb1a70849f7853ed811cb1779bc4f229f662f0
CatRass <catrassyt@gmail.com>
2023-01-12 19:52:47 +1100

    Improving graphics and visual appearance
    First off, some GUI overhauls:
       - Created a basic style sheet
       - Implemented custom font usage (Currently using VT323)
    
    With some additional backend improvments:
       - The additon of scraping and displaying one image for any given SCP entry (One in the <div> class "scp-image-block block-right")
       - Reworking the text display system to prevent it from displaying some unnecessary entries, such as "None", "Filename:" and " Licensing Disclosures".
    
    The only change that is not related to the above theme of the "Graphics" update is the addition to the reGen script to use UTF-8 Encoding to avoid previous errors.

--- main.py
+++ main.py
@@ -28,16 +28,19 @@
 #               Created by Darrel (CatRass#5748)
 #
 #               Original Creation Date: 29/11/22
-#                   Last Edited: 15/12/22
+#                   Last Edited: 12/01/23
 
 import sys
 from PyQt6.QtCore import QSize, Qt
 from PyQt6 import sip
 from PyQt6.QtWidgets import *
-from PyQt6.QtGui import QPalette, QColor, QIcon
+from PyQt6.QtGui import QPalette, QColor, QIcon, QFont, QFontDatabase, QImage, QPixmap
+
 import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
+import inspect
 
 def SCPScraper(scp):
 
@@ -57,7 +60,20 @@ def SCPScraper(scp):
     allContents = {}
     currentKey = "None" #   In case text doesnt have a heading, the previous one will be used
 
+    removableText = [
+    "« SCP-{previous} | SCP-{current} | SCP-{next} »".format(previous="00"+str(int(scp)-1),current="00"+str(int(scp)),next="00"+str(int(scp)+1))]
+    # print(removableText)
+
+    def textRemover(currentEntry):
+        for i in removableText:
+            # print(i)
+            if i == currentEntry:
+                currentEntry = "Cring"
+
     for i in range(0,len(relevantPageContents)):
+
+        # TODO: Fix issues with similar non-none <strong></strong> headings being put into one variable such as the case for SCP-163
+
 
         try:    #   Not every piece of text will have a heading, hence a try except statement in case it does not.
             currentTitle = relevantPageContents[i].find('strong')   #   If a heading is found add to variable
@@ -69,14 +85,30 @@ def SCPScraper(scp):
         if currentTitle != None:
             for strong in currentText.find_all('strong'): strong.extract()
             for pTags in currentText.find_all("p"):    #   Find all contents with the <p></p> tag
-                currentText= currentText.get_text() #   Remove <p></p> tags
+                currentText = currentText.get_text() #   Remove <p></p> tags
+
+            # textRemover(currentText.text)
+            # print(currentText)
+
             allContents[currentTitle.text]=currentText.text #   Append text into dictionary
             currentKey = currentTitle.text  #   Set the previous key in case next item has no heading
         else:
             try:    #   In case this is the first item without a heading, we use try/except
+                # textRemover(currentText)
+                # print(currentText)
                 allContents[currentKey]+=(currentText.text+"\n\n")  #   Add to previous heading
             except:
+                # textRemover(currentText)
+                # print(currentText)
                 allContents[currentKey]=currentText.text    #   If no previous heading, create new one with currrentKey
+
+    try:
+        imageContents = soup.find("div", { "class" : "scp-image-block block-right" })
+        relevantImageContents = imageContents.find("img", { "class" : "image" })
+        print(relevantImageContents["src"])
+        allContents["Image"] = relevantImageContents["src"]
+    except:
+        allContents["Image"] = None
 
     print("SCP {} Scraped Info:".format(str(scp)),allContents)
     return allContents
@@ -110,23 +142,29 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("SCP Database")
+        self.setMinimumSize(QSize(400, 300))
+
         self.setWindowIcon(QIcon('icon.ico'))
         self.toolSelector = toolSelector(["SCP's","Tales"])
         self.scpInput = inputBar("Enter SCP Number",self.scpSearched)
+        self.scpInput.setFont(QFont("VT323",15))
+
+        self.button = QPushButton("Search", self)
+        self.button.setFixedSize(120, 30)
+        self.button.setFont(QFont("VT323",15))
+        self.button.clicked.connect(self.scpSearched)
 
         self.scpSearchedBool = False
 
-        self.setWindowTitle("SCP Database")
-        #self.setFixedSize(QSize(400, 300))
-
-        self.button = QPushButton("Search", self)
-        self.button.setFixedSize(60, 30)
-        self.button.clicked.connect(self.scpSearched)
-
         self.layout = QGridLayout()
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         secondaryLayout = QVBoxLayout()
         self.layout.addWidget(self.scpInput, 0,0)
         self.layout.addWidget(self.button, 0,1)
+
+        self.imageLabel = QLabel(self)
+        self.layout.addWidget(self.imageLabel, 1,1)
 
         self.widget = QWidget()
         self.widget.setLayout(self.layout)
@@ -136,20 +174,56 @@ class MainWindow(QMainWindow):
 
         scpInfo = SCPScraper(self.scpInput.text())  #   Store SCPScraper dictionary in a variable
                                                     #   ↑   scpInfo = {"Heading":"text","Heading2":"text"...}
-        self.scpInformation = QWidget() #   Widget to store all SCP Information
+        # self.scpInformation = QWidget() #   Widget to store all SCP Information
         self.scpInfoTextBrowser = QTextEdit(readOnly = False)
         self.scpInfoTextBrowser.clear()
+        # self.scpInfoTextBrowser.setStyleSheet("color: grey; background-color: #333232")
 
         index = 1
         for key in scpInfo:
+            self.scpInfoTextBrowser.setFont(QFont("VT323")) #   Sets the text font
 
-            self.scpInfoTextBrowser.append(key)
-            self.scpInfoTextBrowser.append(scpInfo[key])
-            self.scpInfoTextBrowser.append('')
+            if key == "Image":
+                if scpInfo[key] != None:    #   Check if the image exists
 
-            index += 1
+                    data = urllib.request.urlopen(scpInfo[key]).read()
+                    image = QImage()
+                    image.loadFromData(data)
+                    imagePixMap = QPixmap(image)
+                    imagePixMap= imagePixMap.scaled(256, 256, Qt.AspectRatioMode.KeepAspectRatio)   #   Resize the image keeping the aspect ratio
+
+                    self.imageLabel.setPixmap(imagePixMap)
+                    self.show()
+                else:
+                    self.imageLabel.clear() #   Clear the label if no image exists
+
+            # ====  THIS IS A TEMPORARY SOLUTION TO REMOVING SOME UNFILTERED INFO   ====
+            # TODO: Optimise this jazz
+            elif key == "None":
+                print("None Element:\n",scpInfo[key])
+            elif key == "Filename:":
+                print("Filename:\n",scpInfo[key])
+            elif key == "Licensing Disclosures":
+                print("Licensing Disclosures:\n",scpInfo[key])
+            # ====  THIS IS A TEMPORARY SOLUTION TO REMOVING SOME UNFILTERED INFO   ====
+
+            else:   #   If the entry in allContents is just regular text, put it in the TextBrowser
+                self.scpInfoTextBrowser.setFontPointSize(20)
+                self.scpInfoTextBrowser.setTextColor(QColor('Red'))
+                self.scpInfoTextBrowser.setFontUnderline(True)
+                self.scpInfoTextBrowser.append(key)
+
+                self.scpInfoTextBrowser.setFontPointSize(15)
+                self.scpInfoTextBrowser.setTextColor(QColor('white'))
+                self.scpInfoTextBrowser.setFontUnderline(False)
+                self.scpInfoTextBrowser.append(scpInfo[key])
+
+                self.scpInfoTextBrowser.append('')
+
+                index += 1
 
         self.layout.addWidget(self.scpInfoTextBrowser,1,0)
+
         self.scpInfoTextBrowser.setReadOnly(True)
 
         self.scpSearchedBool = True #   Indicate that the page has contents on it
@@ -158,4 +232,13 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
+
+QFontDatabase.addApplicationFont("/Styles/Fonts/VT323-Regular.ttf") #   Load the font from the .TTF file
+
+stylesheet = open("Styles/stylesheet.css")  #   Load the Style Sheet from the .CSS file
+style = stylesheet.read()
+
+app.setStyleSheet(style)    #   Set the style
+
+
 app.exec()
