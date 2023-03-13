b06022e4205684e0082b518829e8045cc26523f3
CatRass <catrassyt@gmail.com>
2023-03-13 13:34:00 +1100

    Slight code optimisation and modularisation Not a very big commit, but a good one to start off with after going on hiatus.    - Optimised some code for console-outputting unnecessary text    - Added a "Modules" folder       - Modularised the "SCPScraper" function into a module That is all for now.

--- main.py
+++ main.py
@@ -28,90 +28,19 @@
 #               Created by Darrel (CatRass#5748)
 #
 #               Original Creation Date: 29/11/22
-#                   Last Edited: 12/01/23
+#                   Last Edited: 12/03/23
 
 import sys
 from PyQt6.QtCore import QSize, Qt
 from PyQt6 import sip
-from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import * 
 from PyQt6.QtGui import QPalette, QColor, QIcon, QFont, QFontDatabase, QImage, QPixmap
-
 import requests
 import urllib.request
-from bs4 import BeautifulSoup
 
-import inspect
+from Modules import SCPScraper
 
-def SCPScraper(scp):
-
-        #   This function is responsible for scraping the SCP Wiki and collecting all the relevant information for displaying it on the database GUI. Currently it collects:
-        #       - Headings  (<strong></strong>)
-        #       - Paragaphs/Text (<p></p>)
-        #       - Image Captions
-        #   This is accomplished through grabbing a specific pages source code using Requests, and breaking the scraped code with BeautifulSoup
-
-    url = 'https://scp-wiki.wikidot.com/scp-{}'.format(scp)
-    htmlSource = requests.get(url)  #   The sites HTML is scraped
-    soup = BeautifulSoup(htmlSource.text, 'html.parser')    #   htmlSource is transformed into a bs4 object for analysis
-    pageContents = soup.find("div", { "id" : "page-content" })  #   The webpage is redyuced to everything within div "page-content"
-                                                                #   ↑ Refer to Folio (Analysis → Research → SCP Wiki Article Structure)
-    relevantPageContents = pageContents.find_all("p")    #   Find all contents with the <p></p> tag
-
-    allContents = {}
-    currentKey = "None" #   In case text doesnt have a heading, the previous one will be used
-
-    removableText = [
-    "« SCP-{previous} | SCP-{current} | SCP-{next} »".format(previous="00"+str(int(scp)-1),current="00"+str(int(scp)),next="00"+str(int(scp)+1))]
-    # print(removableText)
-
-    def textRemover(currentEntry):
-        for i in removableText:
-            # print(i)
-            if i == currentEntry:
-                currentEntry = "Cring"
-
-    for i in range(0,len(relevantPageContents)):
-
-        # TODO: Fix issues with similar non-none <strong></strong> headings being put into one variable such as the case for SCP-163
-
-
-        try:    #   Not every piece of text will have a heading, hence a try except statement in case it does not.
-            currentTitle = relevantPageContents[i].find('strong')   #   If a heading is found add to variable
-        except:
-            currentTitle = None #   If a heading not found, make None
-
-        currentText = relevantPageContents[i]   #   Set text to the current item
-
-        if currentTitle != None:
-            for strong in currentText.find_all('strong'): strong.extract()
-            for pTags in currentText.find_all("p"):    #   Find all contents with the <p></p> tag
-                currentText = currentText.get_text() #   Remove <p></p> tags
-
-            # textRemover(currentText.text)
-            # print(currentText)
-
-            allContents[currentTitle.text]=currentText.text #   Append text into dictionary
-            currentKey = currentTitle.text  #   Set the previous key in case next item has no heading
-        else:
-            try:    #   In case this is the first item without a heading, we use try/except
-                # textRemover(currentText)
-                # print(currentText)
-                allContents[currentKey]+=(currentText.text+"\n\n")  #   Add to previous heading
-            except:
-                # textRemover(currentText)
-                # print(currentText)
-                allContents[currentKey]=currentText.text    #   If no previous heading, create new one with currrentKey
-
-    try:
-        imageContents = soup.find("div", { "class" : "scp-image-block block-right" })
-        relevantImageContents = imageContents.find("img", { "class" : "image" })
-        print(relevantImageContents["src"])
-        allContents["Image"] = relevantImageContents["src"]
-    except:
-        allContents["Image"] = None
-
-    print("SCP {} Scraped Info:".format(str(scp)),allContents)
-    return allContents
+# =======================================================================================
 
 class Color(QWidget):   #   Debug Widget that shows a coloured square
     def __init__(self, color):
@@ -172,7 +101,7 @@ class MainWindow(QMainWindow):
 
     def scpSearched(self):
 
-        scpInfo = SCPScraper(self.scpInput.text())  #   Store SCPScraper dictionary in a variable
+        scpInfo = SCPScraper.scraper(self.scpInput.text())  #   Store SCPScraper dictionary in a variable
                                                     #   ↑   scpInfo = {"Heading":"text","Heading2":"text"...}
         # self.scpInformation = QWidget() #   Widget to store all SCP Information
         self.scpInfoTextBrowser = QTextEdit(readOnly = False)
@@ -197,15 +126,9 @@ class MainWindow(QMainWindow):
                 else:
                     self.imageLabel.clear() #   Clear the label if no image exists
 
-            # ====  THIS IS A TEMPORARY SOLUTION TO REMOVING SOME UNFILTERED INFO   ====
-            # TODO: Optimise this jazz
-            elif key == "None":
-                print("None Element:\n",scpInfo[key])
-            elif key == "Filename:":
-                print("Filename:\n",scpInfo[key])
-            elif key == "Licensing Disclosures":
-                print("Licensing Disclosures:\n",scpInfo[key])
-            # ====  THIS IS A TEMPORARY SOLUTION TO REMOVING SOME UNFILTERED INFO   ====
+            # Parses current text string and logs what has been skipped over
+            elif key == "None" or key == "Filename:" or key == "Licensing Disclosures":
+                print("{}:\n",scpInfo[key].format(key))
 
             else:   #   If the entry in allContents is just regular text, put it in the TextBrowser
                 self.scpInfoTextBrowser.setFontPointSize(20)
